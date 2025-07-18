675c3dbf8e8feb6dbc894cd1a1f0234e34ae8de9
0xAA55 <72388533+lochidev@users.noreply.github.com>
2025-07-18 12:26:12 +0530

    added tasker import

---
+++ src/Models/Utils.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expandroid.Models
+{
+    public static class Utils
+    {
+        public static string GetTheRealFormat(string format)
+        {
+            format = format.Replace("%Y", "yyyy");
+            format = format.Replace("%m", "MM");
+            format = format.Replace("%b", "MMM");
+            format = format.Replace("%B", "MMMM");
+            format = format.Replace("%h", "MMM");
+            format = format.Replace("%d", "dd");
+            format = format.Replace("%e", "d");
+            format = format.Replace("%a", "ddd");
+            format = format.Replace("%A", "dddd");
+            format = format.Replace("%j", DateTime.Now.DayOfYear.ToString());
+            format = format.Replace("%w", DateTime.Now.DayOfWeek.ToString());
+            format = format.Replace("%u", (DateTime.Now.DayOfWeek + 1).ToString());
+            format = format.Replace("%D", "MM/dd/yyyy");
+            format = format.Replace("%F", "yyyy/MM/dd");
+            format = format.Replace("%H", "HH");
+            format = format.Replace("%I", "hh");
+            format = format.Replace("%p", "tt");
+            format = format.Replace("%M", "mm");
+            format = format.Replace("%S", "ss");
+            format = format.Replace("%R", "HH:mm");
+            format = format.Replace("%T", "HH:mm:ss");
+            format = format.Replace("%r", "hh:mm:ss tt");
+            return format;
+        }
+        public static string GetOriginalFormat(string format)
+        {
+            format = format.Replace("yyyy", "%Y");
+            format = format.Replace("MM", "%m");
+            format = format.Replace("MMM", "%b");
+            format = format.Replace("MMMM", "%B");
+            format = format.Replace("dd", "%d");
+            format = format.Replace("d", "%e");
+            format = format.Replace("ddd", "%a");
+            format = format.Replace("dddd", "%A");
+            format = format.Replace("MM/dd/yyyy", "%D");
+            format = format.Replace("yyyy/MM/dd", "%F");
+            format = format.Replace("HH", "%H");
+            format = format.Replace("hh", "%I");
+            format = format.Replace("tt", "%p");
+            format = format.Replace("mm", "%M");
+            format = format.Replace("ss", "%S");
+            format = format.Replace("HH:mm", "%R");
+            format = format.Replace("HH:mm:ss", "%T");
+            format = format.Replace("hh:mm:ss tt", "%r");
+            return format;
+        }
+    }
+}
---
+++ src/Platforms/Android/ConfigImportReceiver.cs
@@ -0,0 +1,85 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using CommunityToolkit.Mvvm.Messaging;
+using Expandroid.Models;
+using System.Text.Json;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+using static Kotlin.Collections.Builders.MapBuilder;
+
+[BroadcastReceiver(Enabled = true, Exported = true)]
+[IntentFilter(new[] { "com.dingleinc.texttoolspro.IMPORT_CONFIG" })]
+public class ConfigImportReceiver : BroadcastReceiver
+{
+    // Tasker and MacroDroid package names
+    private static readonly string[] AllowedPackages = new[]
+    {
+        "net.dinglisch.android.taskerm",    // Tasker
+        "com.arlosoft.macrodroid"           // MacroDroid
+    };
+    private void SendMessage(string cmd, Match value)
+    {
+        WeakReferenceMessenger.Default.Send(new AcServiceMessage((cmd, value)));
+    }
+    public override void OnReceive(Context context, Intent intent)
+    {
+        // Only check on Android 4.4+ (API 19+)
+        if (Build.VERSION.SdkInt >= BuildVersionCodes.JellyBeanMr1)
+        {
+            int callingUid = Binder.CallingUid;
+            var pm = context.PackageManager;
+            var packages = pm.GetPackagesForUid(callingUid);
+            if (packages == null || !packages.Any(pkg => AllowedPackages.Contains(pkg)))
+                return;
+        }
+
+        var configStr = intent.GetStringExtra("config_string");
+        if (!string.IsNullOrEmpty(configStr))
+        {
+            File.WriteAllText(AppSettings.DictPath, configStr);
+            var deserializer = new DeserializerBuilder()
+                        .WithNamingConvention(UnderscoredNamingConvention.Instance).IgnoreUnmatchedProperties()
+                        .Build();
+            var localDict = deserializer.Deserialize<DictWrapper>(configStr);
+            foreach (var item in localDict.Matches)
+            {
+                if (item.Vars is not null)
+                {
+                    bool notSupported = item.Replace is null;
+                    foreach (var x in item.Vars)
+                    {
+                        if (x.Type is not null)
+                        {
+                            if (!AppSettings.SupportedList.Contains(x.Type))
+                            {
+                                notSupported = true;
+                                break;
+                            }
+                            else if (x.Type == "date")
+                            {
+                                try
+                                {
+                                    x.Params.Format = Utils.GetTheRealFormat(x.Params.Format);
+                                }
+                                catch (Exception)
+                                {
+                                    throw new Exception("Please make sure date extension parameter formats are present!");
+                                }
+                            }
+                        }
+                    }
+                    if (notSupported)
+                        continue;
+                }
+                SendMessage("Add", item);
+            }
+            if (localDict.Global_vars is not null)
+            {
+                var str = JsonSerializer.Serialize(localDict.Global_vars);
+                File.WriteAllText(AppSettings.GlobalVarsPath, str);
+                WeakReferenceMessenger.Default.Send(new AcGlobalsMessage(localDict.Global_vars));
+            }
+        }
+    }
+}
