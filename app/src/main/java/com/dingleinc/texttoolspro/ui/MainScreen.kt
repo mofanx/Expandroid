61349df2681950d4ac8d5850f60f4c1b7268017b
mofanx <yanwuning@live.cn>
2026-06-21 14:27:18 +0800

    fix: 导入 MainActivity 修复 Unresolved reference

--- app/src/main/java/com/dingleinc/texttoolspro/ui/MainScreen.kt
+++ app/src/main/java/com/dingleinc/texttoolspro/ui/MainScreen.kt
@@ -45,6 +45,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import com.dingleinc.texttoolspro.MainActivity
 import com.dingleinc.texttoolspro.R
 import com.dingleinc.texttoolspro.data.Match
 import com.dingleinc.texttoolspro.data.Var
